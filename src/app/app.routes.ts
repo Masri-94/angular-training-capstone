be5c808417919933a826e2e73096468769a07307
Masri-94 <dimamasri114@gmail.com>
2026-07-03 03:51:48 +0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+    path: '',
+    component: Home ,
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    component: NotFound
+  }
+
+];
